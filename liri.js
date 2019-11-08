ffb6882076dee8dc85a5ea88122a43b35c5a4e2b
Eddie DePaolo <edepaolo@gmail.com>
2019-11-08 14:28:37 -0800

    renamed key.js to keys.js and created starter code of for concert-this function

--- liri.js
+++ liri.js
@@ -1,5 +1,30 @@
 require("dotenv").config();
 
-var keys = require("./keys.js");
+var keys = require("./keys");
 
-var spotify = new Spotify(keys.spotify);
+// var spotify = new Spotify(keys.spotify);
+
+// var Spotify = require('node-spotify-api');
+
+var axios = require('axios');
+
+var moment = require('moment');
+
+var fs = require('fs');
+
+var liri = {
+    'concert-this': function (artist) {
+        axios.get('https://rest.bandsintown/artists/' + artist + '/events?app_id=codingbootcamp')
+            .then(response => {
+                for (var i = 0; i < response.data.length; ++i) {
+                    var data = response.data[i];
+                    var venue = data.venue;
+                    console.log('This function works.')
+                    console.log('Venue Name: ' + venue.name);
+                    console.log('City: ' + venue.city + ',' + venue.region);
+                    console.log('Date: ' + moment(data.datetime).format('MM/DD/YY'));
+                }
+            })
+    }
+
+}
