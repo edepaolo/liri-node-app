952a949adbbcd085bf1e4c00ed429d52baa13ff5
Eddie DePaolo <edepaolo@gmail.com>
2019-11-08 15:03:19 -0800

    fixed typo in axios.get url and added runCommand key/value

--- liri.js
+++ liri.js
@@ -14,17 +14,30 @@ var fs = require('fs');
 
 var liri = {
     'concert-this': function (artist) {
-        axios.get('https://rest.bandsintown/artists/' + artist + '/events?app_id=codingbootcamp')
+        axios.get("https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp")
             .then(response => {
                 for (var i = 0; i < response.data.length; ++i) {
                     var data = response.data[i];
                     var venue = data.venue;
-                    console.log('This function works.')
+                    console.log('this function works')
                     console.log('Venue Name: ' + venue.name);
                     console.log('City: ' + venue.city + ',' + venue.region);
                     console.log('Date: ' + moment(data.datetime).format('MM/DD/YY'));
                 }
-            })
+            });
+    },
+    
+    runCommand: function (str) {
+        var commaIndex = str.indexOf(',');
+        var command, param;
+        
+        if (commaIndex > -1) {
+            command = str.slice(0, commaIndex);
+            param = str.slice(commaIndex + 1);
+        } else command = str;
+        console.log(command);
+        this[command](param);
+        
     }
-
 }
+liri.runCommand(process.argv.slice(2).join(','));
